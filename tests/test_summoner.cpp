ce4adcf37707d086850e56a85b262d0e2e53ec6b
Alias10294 <verzeleflorian@gmail.com>
2025-06-22 20:41:35 +0200

    feat: added test for heim::summoner

---
+++ tests/test_summoner.cpp
@@ -0,0 +1,23 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+
+#include "heim/summoner.hpp"
+
+TEST_CASE("summoner: creates entities")
+{
+  heim::summoner s;
+  heim::entity e1 = s.summon();
+  heim::entity e2 = s.summon();
+  CHECK(e1 == 0);
+  CHECK(e2 == 1);
+}
+
+TEST_CASE("summoner: recycles entities")
+{
+  heim::summoner s;
+  heim::entity e1 = s.summon();
+  s.banish(e1);
+  heim::entity e2 = s.summon();
+  CHECK(e1 == e2);
+}
