f067c117183a77c346a98fe9a357522371a05f67
Alias10294 <verzeleflorian@gmail.com>
2025-08-12 19:03:31 +0200

    feat: tests for summoner

---
+++ test/entity/test_summoner.cpp
@@ -0,0 +1,33 @@
+#include <cstdint>
+#include "../doctest.h"
+#include "../../include/entity/summoner.hpp"
+
+TEST_CASE("heim::summoner")
+{
+  heim::summoner<std::uint32_t> summoner;
+
+  CHECK(summoner.summon() == 0);
+  CHECK(summoner.summon() == 1);
+  CHECK(summoner.summon() == 2);
+  CHECK(summoner.summon() == 3);
+
+  summoner.banish(0);
+  CHECK(summoner.summon() == 0);
+  CHECK(summoner.summon() == 4);
+
+  summoner.banish(0);
+  summoner.banish(1);
+  summoner.banish(2);
+  summoner.banish(3);
+  summoner.banish(4);
+
+  CHECK(summoner.summon() == 4);
+  CHECK(summoner.summon() == 3);
+  CHECK(summoner.summon() == 2);
+  CHECK(summoner.summon() == 1);
+  CHECK(summoner.summon() == 0);
+
+  summoner.banish(2);
+  CHECK(summoner.summon() == 2);
+  CHECK(summoner.summon() == 5);
+}
