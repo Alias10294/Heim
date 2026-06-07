97793e1934766158442786e988577f6812577483
Alias10294 <verzeleflorian@gmail.com>
2026-06-07 16:43:14 +0200

    refactor: started serious adjacency_list implementation

--- include/heim/lib/graph/structure/adjacency_list.hpp
+++ include/heim/lib/graph/structure/adjacency_list.hpp
@@ -3,226 +3,59 @@
 
 #include <cstddef>
 #include <memory>
-#include <ranges>
-#include <utility>
 #include <vector>
-
-#include "heim/lib/graph/interface/primitives.hpp"
+#include "heim/lib/utility.hpp"
 
 namespace heim::graphs
 {
+namespace detail
+{
+template<typename T>
+struct adjacency_list_vertex_descriptor
+{ T value; };
+
+
 template<
-    typename Direction,
-    typename UInt      = std::size_t,
-    typename Allocator = std::allocator<UInt>>
-class adjacency_list
+    typename T,
+    bool     IsOutIncident,
+    bool     IsInIncident>
+struct adjacency_list_edge_descriptor
 { };
 
+template<typename T> struct adjacency_list_edge_descriptor<T, false, false> { T value; };
+template<typename T> struct adjacency_list_edge_descriptor<T, true , false> { T value, head; };
+template<typename T> struct adjacency_list_edge_descriptor<T, false, true > { T value, tail; };
+template<typename T> struct adjacency_list_edge_descriptor<T, true , true > { T value, tail, head; };
 
+} // namespace detail
+
+
+/*!
+ * \brief
+ *   ...
+ */
 template<
-    typename UInt,
-    typename Allocator>
-class adjacency_list<
-    undirected_graph_tag,
-    UInt,
-    Allocator>
+    bool     IsDirected,
+    bool     IsMultigraph,
+    bool     IsOutIncident = true,
+    bool     IsInIncident  = true,
+    typename T             = std::size_t,
+    typename Allocator     = std::allocator<T>>
+requires std::unsigned_integral<T>
+      && allocator_for         <Allocator, T>
+class adjacency_list
 {
 public:
-  using direction_type = undirected_graph_tag;
-  using unsigned_type  = UInt;
+  static constexpr bool is_directed     = IsDirected;
+  static constexpr bool is_multigraph   = IsMultigraph;
+  static constexpr bool is_out_incident = IsOutIncident;
+  static constexpr bool is_in_incident  = IsInIncident;
+
+  using value_type     = T;
   using allocator_type = Allocator;
 
-  struct vertex_descriptor { friend adjacency_list; private: unsigned_type value; };
-  struct edge_descriptor   { friend adjacency_list; private: unsigned_type value, tail, head; };
-
-private:
-  using alloc_traits
-  = std::allocator_traits<allocator_type>;
-
-
-  struct out_edge_descriptor { friend adjacency_list; private: unsigned_type value, head; };
-
-  using out_edge_allocator = typename alloc_traits::template rebind_alloc<out_edge_descriptor>;
-  using out_edge_container = std::vector<out_edge_descriptor, out_edge_allocator>;
-
-  using vertex_allocator = typename alloc_traits::template rebind_alloc<out_edge_container>;
-  using vertex_container = std::vector<out_edge_container, vertex_allocator>;
-
-  using vertex_pair    = std::pair<unsigned_type, unsigned_type>;
-  using edge_allocator = typename alloc_traits::template rebind_alloc<vertex_pair>;
-  using edge_container = std::vector<vertex_pair, edge_allocator>;
-
-private:
-  vertex_container m_vertices;
-  edge_container   m_edges;
-
-private:
-  static constexpr
-  bool
-  s_noexcept_default_construct()
-  noexcept
-  {
-    return std::is_nothrow_default_constructible_v<vertex_allocator>
-        && std::is_nothrow_default_constructible_v<edge_allocator>;
-  }
-
-  static constexpr
-  bool
-  s_noexcept_move_alloc_construct()
-  noexcept
-  {
-    return
-        std::is_nothrow_constructible_v<
-            vertex_container,
-            vertex_container &&, vertex_allocator>
-     && std::is_nothrow_constructible_v<
-            edge_container,
-            edge_container &&, edge_allocator>;
-  }
-
-  static constexpr
-  bool
-  s_noexcept_swap()
-  noexcept
-  {
-    return std::is_nothrow_swappable_v<vertex_container>
-        && std::is_nothrow_swappable_v<edge_container>;
-  }
-
-public:
-  explicit constexpr
-  adjacency_list(allocator_type const &alloc)
-  noexcept
-    : m_vertices{vertex_allocator{alloc}}
-    , m_edges   {edge_allocator  {alloc}}
-  { }
-
-  constexpr
-  adjacency_list()
-  noexcept(s_noexcept_default_construct())
-    : adjacency_list{allocator_type{}}
-  { }
-
-  constexpr
-  adjacency_list(adjacency_list const &other, allocator_type const &alloc)
-    : m_vertices{other.m_vertices, vertex_allocator{alloc}}
-    , m_edges   {other.m_edges   , edge_allocator  {alloc}}
-  { }
-
-  constexpr
-  adjacency_list(adjacency_list const &other)
-  = default;
-
-  constexpr
-  adjacency_list(adjacency_list &&other, allocator_type const &alloc)
-  noexcept(s_noexcept_move_alloc_construct())
-    : m_vertices{std::move(other.m_vertices), vertex_allocator{alloc}}
-    , m_edges   {std::move(other.m_edges   ), edge_allocator  {alloc}}
-  { }
-
-  constexpr
-  adjacency_list(adjacency_list &&other)
-  = default;
-
-  constexpr
-  ~adjacency_list()
-  = default;
-
-  constexpr
-  adjacency_list &
-  operator=(adjacency_list const &other)
-  = default;
-
-  constexpr
-  adjacency_list &
-  operator=(adjacency_list &&other)
-  = default;
-
-  [[nodiscard]]
-  constexpr
-  allocator_type
-  get_allocator() const
-  noexcept
-  { return allocator_type{m_vertices.get_allocator()}; }
-
-  constexpr
-  void
-  swap(adjacency_list &other)
-  noexcept(s_noexcept_swap())
-  {
-    std::swap(m_vertices, other.m_vertices);
-    std::swap(m_edges   , other.m_edges);
-  }
-
-  friend constexpr
-  void
-  swap(adjacency_list &lhs, adjacency_list &rhs)
-  noexcept(s_noexcept_swap())
-  { lhs.swap(rhs); }
-
-
-  [[nodiscard]]
-  constexpr
-  auto
-  vertices() const
-  noexcept
-  {
-    return std::views::iota(
-               unsigned_type{},
-               unsigned_type{m_vertices.size()})
-         | std::views::transform(
-               [](unsigned_type const val)
-               {
-                 return vertex_descriptor{val};
-               });
-  }
-
-  [[nodiscard]]
-  constexpr
-  auto
-  edges() const
-  noexcept
-  {
-    return std::views::iota(
-               unsigned_type{},
-               unsigned_type{m_edges.size()})
-         | std::views::transform(
-               [this](unsigned_type const val)
-               {
-                 auto   ends           {m_edges[val]};
-                 return edge_descriptor{val, ends.first, ends.second};
-               });
-  }
-
-  [[nodiscard]]
-  constexpr
-  auto
-  out_edges(vertex_descriptor const v) const
-  noexcept
-  {
-    unsigned_type tail{v.value};
-
-    return m_vertices[tail]
-         | std::views::transform(
-               [tail](out_edge_descriptor const e)
-               {
-                 return edge_descriptor{e.value, tail, e.head};
-               });
-  }
-
-  [[nodiscard]]
-  constexpr
-  vertex_descriptor
-  head(edge_descriptor const e) const
-  noexcept
-  { return vertex_descriptor{e.head}; }
-
-  [[nodiscard]]
-  constexpr
-  vertex_descriptor
-  tail(edge_descriptor const e) const
-  noexcept
-  { return vertex_descriptor{e.tail}; }
+  using vertex_descriptor = detail::adjacency_list_vertex_descriptor<value_type>;
+  using edge_descriptor   = detail::adjacency_list_edge_descriptor  <value_type, is_out_incident, is_in_incident>;
 };
 
 } // namespace heim::graphs
